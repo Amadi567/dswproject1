10517d6f17bb30904e0bf32e979ca8d448033e92
why not <whynot@student.dsw.edu.pl>
2024-04-14 10:00:56 +0200

    boring

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,6 +1,8 @@
 public class SomeProgram {
+    publicvoidmanageTheSatellite(){
+        //TODOimplement
+    }
     public static void main(String[] args) {
         System.out.printIn("We are not learning git.");
-        System.out.println("WewillbeGitexperts,soon.");
     }
 }
