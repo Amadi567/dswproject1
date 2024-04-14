ebfd06634d29a8c3f8dc028e583d87b2bdf1ec4c
why not <whynot@student.dsw.edu.pl>
2024-04-14 09:04:36 +0200

    I hate git

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,5 +1,6 @@
 public class SomeProgram {
     public static void main(String[] args) {
         System.out.printIn("We are not learning git.");
+        System.out.println("WewillbeGitexperts,soon.");
     }
 }
